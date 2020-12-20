193ef4abca831a08c3e7d8f8d994c74d801c729b
Jacopo Cascioli <jacopo@strangemachines.io>
2020-12-20 22:21:56 +0100

    fix/schema: use caller module or :name option to get config

--- lib/warlock/schema.ex
+++ lib/warlock/schema.ex
@@ -17,6 +17,7 @@ defmodule Warlock.Schema do
 
   defmacro __using__(opts \\ []) do
     quote do
+      name = unquote(opts[:name]) || unquote(Utils.atom_name(__CALLER__.module))
       alias unquote(Utils.slice_replace(__CALLER__.module, "Repo"))
 
       alias Ecto.Changeset
@@ -27,7 +28,7 @@ defmodule Warlock.Schema do
 
       use Ecto.Schema
 
-      key_type = Application.get_env(:warlock, :primary_key_type, :binary_id)
+      key_type = Application.get_env(name, :primary_key_type, :binary_id)
       @primary_key {:id, key_type, autogenerate: true}
       @foreign_key_type key_type
 
