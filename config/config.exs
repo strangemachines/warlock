5d96c2560ca5010972e29a47499351f56f41723d
nomorepanic <nomorepanic@strangemachines.io>
2021-04-10 18:06:55 +0200

    feat/config: add auth_* options

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,9 @@
 import Config
 
 config :warlock,
+  auth_challenge: "bearer",
+  auth_error: "invalid_credentials",
+  auth_realm: "warlock",
   items_per_page: "20",
   port: 8000,
   primary_key_type: :binary_id,
