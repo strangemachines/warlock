67afd8baf4d60d8bcf395529c1f11ed5a7053ec7
Jacopo Cascioli <jacopo@strangemachines.io>
2021-04-12 00:51:22 +0200

    feat/config: add messages option

--- config/config.exs
+++ config/config.exs
@@ -7,4 +7,17 @@ config :warlock,
   items_per_page: "20",
   port: 8000,
   primary_key_type: :binary_id,
-  response_type: :json
+  response_type: :json,
+  messages: [
+    accepted: "accepted",
+    bad_request: "bad request",
+    unauthorized: "unauthorized",
+    forbidden: "forbidden",
+    not_found: "not found",
+    not_allowed: "not allowed",
+    conflict: "conflict",
+    unsupported: "unsupported",
+    unprocessable: "unprocessable",
+    internal_error: "unknown",
+    not_implemented: "not implemented"
+  ]
