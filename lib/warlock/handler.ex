ee88b2083fce84121bc02235df74bbc071a1ba75
nomorepanic <nomorepanic@strangemachines.io>
2023-07-02 19:11:42 +0200

    feat/handler/builder: add Builder module

--- lib/warlock/handler.ex
+++ lib/warlock/handler.ex
@@ -1,5 +1,4 @@
 defmodule Warlock.Handler do
-  alias Warlock.{Handler, Siren}
   alias Warlock.ModuleUtils, as: Utils
   alias Plug.Conn
 
@@ -15,112 +14,14 @@ defmodule Warlock.Handler do
 
   @optional_callbacks get: 1, new: 1, show: 2, edit: 2, delete: 2
 
-  defmacro json_payload(code) do
-    quote do
-      def unquote(:"send_#{code}")(conn, payload) do
-        Handler.json(conn, unquote(code), payload)
-      end
-    end
-  end
-
-  defmacro json_error(code, default_error) do
-    quote do
-      def unquote(:"send_#{code}")(conn) do
-        Handler.message(conn, unquote(code), unquote(default_error))
-      end
-
-      def unquote(:"send_#{code}")(conn, error) do
-        Handler.json(conn, unquote(code), error)
-      end
-    end
-  end
-
-  defmacro siren_payload(code) do
-    quote do
-      def unquote(:"send_#{code}")(conn, payload) do
-        Handler.siren(conn, unquote(code), Siren.encode(conn, payload))
-      end
-
-      def unquote(:"send_#{code}")(conn, payload, count) do
-        Handler.siren(conn, unquote(code), Siren.encode(conn, payload, count))
-      end
-    end
-  end
-
-  defmacro siren_error(code, default_error) do
-    quote do
-      def unquote(:"send_#{code}")(conn) do
-        error =
-          Siren.error(unquote(code), [],
-            class: ["error", unquote(default_error)],
-            summary: unquote(default_error)
-          )
-
-        Handler.siren(conn, unquote(code), error)
-      end
-
-      def unquote(:"send_#{code}")(conn, error) do
-        siren_error =
-          Siren.error(unquote(code), error,
-            class: ["error", unquote(default_error)],
-            summary: unquote(default_error)
-          )
-
-        Handler.siren(conn, unquote(code), siren_error)
-      end
-
-      def unquote(:"send_#{code}")(conn, error, opts) do
-        siren_error = Siren.error(unquote(code), error, opts)
-        Handler.siren(conn, unquote(code), siren_error)
-      end
-    end
-  end
-
-  defmacro payload(code, response_type) do
-    quote do
-      if unquote(response_type) == :siren do
-        siren_payload(unquote(code))
-      else
-        json_payload(unquote(code))
-      end
-    end
-  end
-
-  defmacro error(code, default_error, response_type) do
-    quote do
-      if unquote(response_type) == :siren do
-        siren_error(unquote(code), unquote(default_error))
-      else
-        json_error(unquote(code), unquote(default_error))
-      end
-    end
-  end
-
-  def json(conn, status, payload) do
-    conn
-    |> Conn.put_resp_content_type("application/json")
-    |> Conn.send_resp(status, Jason.encode!(payload))
-  end
-
-  def siren(conn, status, payload) do
-    conn
-    |> Conn.put_resp_content_type("application/vnd.siren+json")
-    |> Conn.send_resp(status, Jason.encode!(payload))
-  end
-
-  def message(conn, status, message) do
-    Handler.json(conn, status, %{:message => message})
-  end
-
   defmacro __using__(opts \\ []) do
     quote do
       name = unquote(Utils.name_or_option(__CALLER__.module, opts[:name]))
 
       alias Plug.Conn
-      alias Warlock.Siren
       alias unquote(Utils.replace_at(__CALLER__.module, "Controllers"))
 
-      import Warlock.Handler
+      import Warlock.Handler.Builder
 
       @behaviour Warlock.Handler
 
@@ -170,47 +71,9 @@ defmodule Warlock.Handler do
       error(501, @not_implemented, @response_type)
 
       if @response_type == :siren do
-        def send_202(conn) do
-          Handler.siren(conn, 202, Siren.message(202, @accepted))
-        end
-
-        def send_401(conn) do
-          error =
-            Siren.error(401, [],
-              class: ["error", @unauthorized],
-              summary: @unauthorized
-            )
-
-          conn
-          |> unquote(__CALLER__.module).authenticate()
-          |> Handler.siren(401, error)
-        end
-
-        def send_401(conn, custom_error) do
-          error =
-            Siren.error(401, custom_error,
-              class: ["error", @unauthorized],
-              summary: @unauthorized
-            )
-
-          conn
-          |> unquote(__CALLER__.module).authenticate()
-          |> Handler.siren(401, error)
-        end
+        build_siren_handler()
       else
-        def send_202(conn), do: Handler.message(conn, 202, @accepted)
-
-        def send_401(conn) do
-          conn
-          |> unquote(__CALLER__.module).authenticate()
-          |> Handler.message(401, @unauthorized)
-        end
-
-        def send_401(conn, error) do
-          conn
-          |> unquote(__CALLER__.module).authenticate()
-          |> Handler.json(401, error)
-        end
+        build_json_handler()
       end
 
       def authenticate(conn) do
