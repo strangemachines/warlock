02cc8b3ef3ffa9ad374d23e68edadb8c5d44f144
nomorepanic <nomorepanic@strangemachines.io>
2023-07-04 01:00:59 +0200

    chore/warlock: version 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Warlock.MixProject do
   def project do
     [
       app: :warlock,
-      version: "1.0.1",
+      version: "2.0.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
